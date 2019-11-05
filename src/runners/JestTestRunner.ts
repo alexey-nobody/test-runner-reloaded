77a4e2714ed8791052554f178cecab9a67df609e
Alexey Lepskii <alexey.lepskii@gmail.com>
2019-11-05 21:41:54 +0300

    Update creating args for running all test with e2e in nest.js

--- src/runners/JestTestRunner.ts
+++ src/runners/JestTestRunner.ts
@@ -1,18 +1,17 @@
-import { join } from "path";
-import { debug, WorkspaceFolder } from "vscode";
-
-import { ITestRunnerInterface } from "../interfaces/ITestRunnerInterface";
-import { ITestRunnerOptions } from "../interfaces/ITestRunnerOptions";
-import { ConfigurationProvider } from "../providers/ConfigurationProvider";
-import { TerminalProvider } from "../providers/TerminalProvider";
+import { join } from 'path';
+import { debug, WorkspaceFolder } from 'vscode';
+import { ITestRunnerInterface } from '../interfaces/ITestRunnerInterface';
+import { ITestRunnerOptions } from '../interfaces/ITestRunnerOptions';
+import { ConfigurationProvider } from '../providers/ConfigurationProvider';
+import { TerminalProvider } from '../providers/TerminalProvider';
 
 export class JestTestRunner implements ITestRunnerInterface {
-  public name: string = "jest";
+  public name: string = 'jest';
   public terminalProvider: TerminalProvider = null;
   public configurationProvider: ConfigurationProvider = null;
 
   get binPath(): string {
-    return join("node_modules", ".bin", "jest");
+    return join('node_modules', '.bin', 'jest');
   }
 
   constructor({ terminalProvider, configurationProvider }: ITestRunnerOptions) {
@@ -20,56 +19,46 @@ export class JestTestRunner implements ITestRunnerInterface {
     this.configurationProvider = configurationProvider;
   }
 
-  public runTest(
-    rootPath: WorkspaceFolder,
-    fileName: string,
-    testName: string
-  ) {
+  public runTest(rootPath: WorkspaceFolder, fileName: string, testName: string) {
     const additionalArguments = this.configurationProvider.additionalArguments;
-    const environmentVariables = this.configurationProvider
-      .environmentVariables;
+    const environmentVariables = this.configurationProvider.environmentVariables;
 
     // We force slash instead of backslash for Windows
-    const cleanedFileName = fileName.replace(/\\/g, "/");
+    const cleanedFileName = fileName.replace(/\\/g, '/');
 
-    const command = `${
-      this.binPath
-    } ${cleanedFileName} --rootDir="." --testRegex=".*.(test|spec|e2e-spec).ts" --testNamePattern="${testName}" ${additionalArguments}`;
+    const mainArgs = `-i ${cleanedFileName} --testNamePattern '${testName}'`;
+    const secondArgs = `--runInBand --testRegex '.*.(test|spec|e2e-spec).ts' --rootDir '.'`;
+    const command = `${this.binPath} ${mainArgs} ${secondArgs} ${additionalArguments}`;
 
-    const terminal = this.terminalProvider.get(
-      { env: environmentVariables },
-      rootPath
-    );
+    const terminal = this.terminalProvider.get({ env: environmentVariables }, rootPath);
 
     terminal.sendText(command, true);
     terminal.show(true);
   }
 
-  public debugTest(
-    rootPath: WorkspaceFolder,
-    fileName: string,
-    testName: string
-  ) {
+  public debugTest(rootPath: WorkspaceFolder, fileName: string, testName: string) {
     const additionalArguments = this.configurationProvider.additionalArguments;
-    const environmentVariables = this.configurationProvider
-      .environmentVariables;
-    // We force slash instead of backslash for Windows
-    const cleanedFileName = fileName.replace(/\\/g, "/");
+    const environmentVariables = this.configurationProvider.environmentVariables;
+    const cleanedFileName = fileName.replace(/\\/g, '/');
+
+    const mainArgs = `-i ${cleanedFileName} --testNamePattern '${testName}'`;
+    const secondArgs = `--runInBand --testRegex '.*.(test|spec|e2e-spec).ts' --rootDir '.'`;
+
+    const formatedArgs = [];
+    formatedArgs.push(...mainArgs.split(' '));
+    formatedArgs.push(...secondArgs.split(' '));
+    if (additionalArguments && additionalArguments.length > 0) {
+      formatedArgs.push(...additionalArguments);
+    }
 
     debug.startDebugging(rootPath, {
-      name: "Debug Test",
-      type: "node",
-      request: "launch",
-      args: [
-        cleanedFileName,
-        `--testNamePattern`,
-        testName,
-        "--runInBand",
-        ...additionalArguments.split(" ")
-      ],
+      name: 'Debug Test',
+      type: 'node',
+      request: 'launch',
+      args: formatedArgs,
       env: environmentVariables,
-      console: "integratedTerminal",
-      program: "${workspaceFolder}/node_modules/jest/bin/jest.js"
+      console: 'integratedTerminal',
+      program: '${workspaceFolder}/node_modules/jest/bin/jest.js',
     });
   }
 }
