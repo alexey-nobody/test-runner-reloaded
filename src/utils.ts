a005e3c4d7b0c1fd53bd81891f3e36a56ea24dc0
Alexey Lepskii <alexey.lepskii@gmail.com>
2020-02-24 11:32:24 +0300

    Add escaping for brackets in the test name

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,6 @@
 export function formatTestName(testName: string) {
-  const formattedTestName = testName.replace(/\s/g, '\\s');
+  let formattedTestName = testName.replace(/\s/g, '\\s');
+  formattedTestName = formattedTestName.replace(/\x28/g, '\\(');
+  formattedTestName = formattedTestName.replace(/\x29/g, '\\)');
   return formattedTestName;
 }
