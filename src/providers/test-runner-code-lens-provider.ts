5cb3e56404f2424e5008e9042e1c57fab5dcbde7
Alexey Lepskii <alexey.lepskii@gmail.com>
2020-03-17 00:17:28 +0300

    Refactoring TestRunnerCodeLensProvider

--- src/providers/test-runner-code-lens-provider.ts
+++ src/providers/test-runner-code-lens-provider.ts
@@ -3,38 +3,36 @@ import { TestDebugRunnerCodeLens } from '../codelens/test-debug-runner-code-lens
 import { TestRunnerCodeLens } from '../codelens/test-runner-code-lens';
 import { codeParser } from '../parser/code-parser';
 
-function getRootPath({ uri }) {
-  const activeWorkspace = workspace.getWorkspaceFolder(uri);
+export class TestRunnerCodeLensProvider implements CodeLensProvider {
+  private getCodeLens(rootPath, fileName, testName, startPosition) {
+    const testRunnerCodeLens = new TestRunnerCodeLens(rootPath, fileName, testName, startPosition);
 
-  if (activeWorkspace) {
-    return activeWorkspace;
+    const testDebugRunnerCodeLens = new TestDebugRunnerCodeLens(
+      rootPath,
+      fileName,
+      testName,
+      startPosition,
+    );
+
+    return [testRunnerCodeLens, testDebugRunnerCodeLens];
   }
 
-  return workspace;
-}
+  private getRootPath({ uri }) {
+    const activeWorkspace = workspace.getWorkspaceFolder(uri);
+    if (activeWorkspace) {
+      return activeWorkspace;
+    }
+    return workspace;
+  }
 
-function getCodeLens(rootPath, fileName, testName, startPosition) {
-  const testRunnerCodeLens = new TestRunnerCodeLens(rootPath, fileName, testName, startPosition);
-
-  const testDebugRunnerCodeLens = new TestDebugRunnerCodeLens(
-    rootPath,
-    fileName,
-    testName,
-    startPosition,
-  );
-
-  return [testRunnerCodeLens, testDebugRunnerCodeLens];
-}
-
-export class TestRunnerCodeLensProvider implements CodeLensProvider {
   public provideCodeLenses(document: TextDocument): CodeLens[] | Thenable<CodeLens[]> {
     const createRangeObject = ({ line }) => document.lineAt(line - 1).range;
-    const rootPath = getRootPath(document);
+    const rootPath = this.getRootPath(document);
 
     return codeParser(document.getText()).reduce(
       (acc, { loc, testName }) => [
         ...acc,
-        ...getCodeLens(rootPath, document.fileName, testName, createRangeObject(loc.start)),
+        ...this.getCodeLens(rootPath, document.fileName, testName, createRangeObject(loc.start)),
       ],
       [],
     );
